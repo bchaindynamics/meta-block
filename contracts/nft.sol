8e6fa52f641c3a50e6da82e8ea7e2a296011b14e
ishitarastogi <ishita3099@gmail.com>
2021-01-27 18:49:20 +0530

    ERC721-mint

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -1,16 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Mintable.sol";
-
-contract Simple721Token is Initializable, ERC721Full, ERC721Mintable {
-
-    function initialize(address sender) public initializer {
-        ERC721.initialize();
-        ERC721Metadata.initialize("Simple721Token", "721");
-        ERC721Enumerable.initialize();
-        ERC721Mintable.initialize(sender);
+contract MyNFT is ERC721Full, ERC721Mintable {
+    constructor() ERC721Full("MyNFT", "MNFT") public {
     }
 }
