3dabbefcce9eb0a3e309504e41b975ba341d132c
ishitarastogi <ishita3099@gmail.com>
2021-01-28 10:09:46 +0530

    change nft

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+
  import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
  import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipientERC20Fee.sol";
@@ -82,3 +83,21 @@ contract Counter is Initializable, GSNRecipientERC20Fee ,ERC20,ERC20Detailed {
   }
 
 }
+// pragma solidity ^0.5.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+// //import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+
+// contract MyNFT is ERC721Full {
+//     // string[] public colors;
+//     // mapping(string => bool) _exist;
+//     constructor() ERC721Full("MyNFT", "MNFT") public {
+//     }
+
+// //     function mint(string memory _color) public {
+// // uint _id=colors.push(_color);
+// // _mint(msg.sender, _id);
+// // _exist[_color]=true;
+// //     }
+
+// }
---
+++ test/kitty.test.js
@@ -0,0 +1,11 @@
+const nft = artifacts.require('./nft.sol')
+require('chai').use(require('chai-as-promised')).should()
+contract('nft', (accounts) => {
+describe('deployment', async() => {
+    it('deploys successfully', async () => {
+        
+    })
+})
+
+
+})
