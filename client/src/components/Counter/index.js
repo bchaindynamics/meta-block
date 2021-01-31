e599824bbe0dfa4f00ff79693dc92a14e0f9f96f
ishitarastogi <ishita3099@gmail.com>
2021-01-31 08:51:25 +0530

    election-dap

--- client/src/components/Counter/index.js
+++ client/src/components/Counter/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { PublicAddress, Button, Loader } from 'rimble-ui';
-
-import styles from './Counter.module.scss';
+//import { PublicAddress, Button, Loader } from 'rimble-ui';
+import Header from './Header';
+import Body from './Body';
+//import styles from './Counter.module.scss';
 
 import getTransactionReceipt from '../../utils/getTransactionReceipt';
 import { utils } from '@openzeppelin/gsn-provider';
@@ -10,7 +11,7 @@ const { isRelayHubDeployedForRecipient, getRecipientFunds } = utils;
 export default function Counter(props) {
   const { instance, accounts, lib, networkName, networkId, providerName } = props;
   const { _address, methods } = instance || {};
-
+  let content;
   // GSN provider has only one key pair
   const isGSN = providerName === 'GSN';
 
@@ -48,34 +49,69 @@ export default function Counter(props) {
     getDeploymentAndFunds();
   }, [getDeploymentAndFunds, instance]);
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const getCount = useCallback(async () => {
-    if (instance) {
-      // Get the value from the contract to prove it worked.
-      const response = await instance.methods.getCounter().call();
-      // Update state with the result.
-      setCount(response);
-    }
-  }, [instance]);
+  // const getCount = useCallback(async () => {
+  //   if (instance) {
+  //     // Get the value from the contract to prove it worked.
+  //     const response = await instance.methods.getCounter().call();
+  //     // Update state with the result.
+  //     setCount(response);
+  //   }
+  // }, [instance]);
 
-  useEffect(() => {
-    getCount();
-  }, [getCount, instance]);
+  // useEffect(() => {
+  //   getCount();
+  // }, [getCount, instance]);
 
   const [sending, setSending] = useState(false);
   const [transactionHash, setTransactionHash] = useState('');
-
-  const increase = async number => {
+  const [candidate1Votes, setCandidate1Votes] = useState();
+  const [candidate2Votes, setCandidate2Votes] = useState();
+  const [winner, setWinner] = useState('');
+  const [loading, setLoading] = useState(true);
+  const increase = async () => {
     try {
       if (!sending) {
         setSending(true);
 
-        const tx = await instance.methods.increaseCounter(number).send({ from: accounts[0] });
-        const receipt = await getTransactionReceipt(lib, tx.transactionHash);
+        const _candidate1 = await instance.methods.candidates(1).call();
+        const _candidate1Votes = Number(_candidate1.voteCount);
+        setCandidate1Votes(_candidate1Votes);
+        const _candidate2 = await instance.methods.candidates(1).call();
+        const _candidate2Votes = Number(_candidate2.voteCount);
+        setCandidate2Votes(_candidate2Votes);
+
+        // const receipt = await getTransactionReceipt(lib, tx.transactionHash);
+        // setTransactionHash(receipt.transactionHash);s
+        if (_candidate1Votes > _candidate2Votes) setWinner('Candidate 1');
+        else if (_candidate1Votes < _candidate2Votes) setWinner('Candidate 2');
+        else setWinner('No one');
+        // getCount();
+        getDeploymentAndFunds();
+        setLoading(false);
+        setSending(false);
+      }
+    } catch (e) {
+      setSending(false);
+      console.log(e);
+    }
+  };
+
+  const castVote = async candidate => {
+    try {
+      if (!sending) {
+        setSending(true);
+        setLoading(true);
+        const receipt = await instance.methods
+          .vote(candidate)
+          .send({ from: accounts })
+          .on('transactionHash', hash => {
+            console.log('Transaction Successful', hash);
+          });
         setTransactionHash(receipt.transactionHash);
 
-        getCount();
+        //getCount();
         getDeploymentAndFunds();
 
         setSending(false);
@@ -86,131 +122,123 @@ export default function Counter(props) {
     }
   };
 
-  const decrease = async number => {
-    try {
-      if (!sending) {
-        setSending(true);
+  // function renderNoDeploy() {
+  //   return (
+  //     <div>
+  //       <p>
+  //         <strong>Can't Load Deployed Counter Instance</strong>
+  //       </p>
+  //       <p>Please, run `oz create` to deploy an counter instance.</p>
+  //     </div>
+  //   );
+  // }
 
-        const receipt = await instance.methods.decreaseCounter(number).send({ from: accounts[0] });
-        setTransactionHash(receipt.transactionHash);
+  // function renderNoFunds() {
+  //   return (
+  //     <div>
+  //       <p>
+  //         <strong>The recipient has no funds</strong>
+  //       </p>
+  //       <p>Please, run:</p>
+  //       <div className={styles.code}>
+  //         <code>
+  //           <small>npx oz-gsn fund-recipient --recipient {_address}</small>
+  //         </code>
+  //       </div>
+  //       <p>to fund the recipient on local network.</p>
+  //     </div>
+  //   );
+  // }
 
-        getCount();
-        getDeploymentAndFunds();
+  // function renderNoBalance() {
+  //   return (
+  //     <div>
+  //       <p>
+  //         <strong>Fund your Metamask account</strong>
+  //       </p>
+  //       <p>You need some ETH to be able to send transactions. Please, run:</p>
+  //       <div className={styles.code}>
+  //         <code>
+  //           <small>openzeppelin transfer --to {accounts[0]}</small>
+  //         </code>
+  //       </div>
+  //       <p>to fund your Metamask.</p>
+  //     </div>
+  //   );
+  // }
 
-        setSending(false);
-      }
-    } catch (e) {
-      setSending(false);
-      console.log(e);
-    }
-  };
-
-  function renderNoDeploy() {
-    return (
-      <div>
-        <p>
-          <strong>Can't Load Deployed Counter Instance</strong>
-        </p>
-        <p>Please, run `oz create` to deploy an counter instance.</p>
-      </div>
+  // function renderTransactionHash() {
+  //   return (
+  //     <div>
+  //       <p>
+  //         Transaction{' '}
+  //         <a
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //           href={`https://${networkName}.etherscan.io/tx/${transactionHash}`}
+  //         >
+  //           <small>{transactionHash.substr(0, 6)}</small>
+  //         </a>{' '}
+  //         has been mined on {networkName} network.
+  //       </p>
+  //     </div>
+  //   );
+  // }
+  if (!loading) {
+    content = (
+      <Body castVote={castVote} winner={winner} candidate1Votes={candidate1Votes} candidate2Votes={candidate2Votes} />
     );
-  }
-
-  function renderNoFunds() {
-    return (
-      <div>
-        <p>
-          <strong>The recipient has no funds</strong>
-        </p>
-        <p>Please, run:</p>
-        <div className={styles.code}>
-          <code>
-            <small>npx oz-gsn fund-recipient --recipient {_address}</small>
-          </code>
-        </div>
-        <p>to fund the recipient on local network.</p>
-      </div>
-    );
-  }
-
-  function renderNoBalance() {
-    return (
-      <div>
-        <p>
-          <strong>Fund your Metamask account</strong>
-        </p>
-        <p>You need some ETH to be able to send transactions. Please, run:</p>
-        <div className={styles.code}>
-          <code>
-            <small>openzeppelin transfer --to {accounts[0]}</small>
-          </code>
-        </div>
-        <p>to fund your Metamask.</p>
-      </div>
-    );
-  }
-
-  function renderTransactionHash() {
-    return (
-      <div>
-        <p>
-          Transaction{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://${networkName}.etherscan.io/tx/${transactionHash}`}
-          >
-            <small>{transactionHash.substr(0, 6)}</small>
-          </a>{' '}
-          has been mined on {networkName} network.
-        </p>
-      </div>
-    );
+  } else {
+    content = <p className="text-center my-4">Loading...</p>;
   }
 
   return (
-    <div className={styles.counter}>
-      <h3> Counter Instance </h3>
-      {lib && !instance && renderNoDeploy()}
-      {lib && instance && (
-        <React.Fragment>
-          <div className={styles.dataPoint}>
-            <div className={styles.label}>Instance address:</div>
-            <div className={styles.value}>
-              <PublicAddress label="" address={_address} />
-            </div>
-          </div>
-          <div className={styles.dataPoint}>
-            <div className={styles.label}>Counter Value:</div>
-            <div className={styles.value}>{count}</div>
-          </div>
-          {isGSN && (
-            <div className={styles.dataPoint}>
-              <div className={styles.label}>Recipient Funds:</div>
-              <div className={styles.value}>{lib.utils.fromWei(funds.toString(), 'ether')} ETH</div>
-            </div>
-          )}
-          {isGSN && !funds && renderNoFunds()}
-          {!isGSN && !balance && renderNoBalance()}
-
-          {(!!funds || !!balance) && (
-            <React.Fragment>
-              <div className={styles.label}>
-                <strong>Counter Actions</strong>
-              </div>
-              <div className={styles.buttons}>
-                <Button onClick={() => increase(1)} size="small">
-                  {sending ? <Loader className={styles.loader} color="white" /> : <span> Increase Counter by 1</span>}
-                </Button>
-                <Button onClick={() => decrease(1)} disabled={!(methods && methods.decreaseCounter)} size="small">
-                  {sending ? <Loader className={styles.loader} color="white" /> : <span> Decrease Counter by 1</span>}
-                </Button>
-              </div>
-            </React.Fragment>
-          )}
-          {transactionHash && networkName !== 'Private' && renderTransactionHash()}
-        </React.Fragment>
-      )}
+    <div>
+      <Header />
+      {content}
     </div>
+    //   <div className={styles.counter}>
+    //     <h3> Counter Instance </h3>
+    //     {lib && !instance && renderNoDeploy()}
+    //     {lib && instance && (
+    //       <React.Fragment>
+    //         <div className={styles.dataPoint}>
+    //           <div className={styles.label}>Instance address:</div>
+    //           <div className={styles.value}>
+    //             <PublicAddress label="" address={_address} />
+    //           </div>
+    //         </div>
+    //         <div className={styles.dataPoint}>
+    //           <div className={styles.label}>Counter Value:</div>
+    //           <div className={styles.value}>{count}</div>
+    //         </div>
+    //         {isGSN && (
+    //           <div className={styles.dataPoint}>
+    //             <div className={styles.label}>Recipient Funds:</div>
+    //             <div className={styles.value}>{lib.utils.fromWei(funds.toString(), 'ether')} ETH</div>
+    //           </div>
+    //         )}
+    //         {isGSN && !funds && renderNoFunds()}
+    //         {!isGSN && !balance && renderNoBalance()}
+
+    //         {(!!funds || !!balance) && (
+    //           <React.Fragment>
+    //             <div className={styles.label}>
+    //               <strong>Counter Actions</strong>
+    //             </div>
+    //             <div className={styles.buttons}>
+    //               <Button onClick={() => increase(1)} size="small">
+    //                 {sending ? <Loader className={styles.loader} color="white" /> : <span> Increase Counter by 1</span>}
+    //               </Button>
+    //               <Button onClick={() => decrease(1)} disabled={!(methods && methods.decreaseCounter)} size="small">
+    //                 {sending ? <Loader className={styles.loader} color="white" /> : <span> Decrease Counter by 1</span>}
+    //               </Button>
+    //             </div>
+    //           </React.Fragment>
+    //         )}
+    //         {transactionHash && networkName !== 'Private' && renderTransactionHash()}
+    //       </React.Fragment>
+    //     )}
+    //   </div>
   );
 }
--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,33 +1,34 @@
 pragma solidity ^0.5.0;
 
- import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
- import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipientERC20Fee.sol";
-//import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
-
-contract Counter is Initializable, GSNRecipientERC20Fee ,ERC20,ERC20Detailed {
-  //it keeps a count to demonstrate stage changesERC20
+contract Counter is Initializable, GSNRecipientERC20Fee {
+  //it keeps a count to demonstrate stage changes
   uint private count;
   address private _owner;
+struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+    uint256 public candidatesCount;
+    mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
+    event votedEvent (
+        uint indexed _candidateId
+    );
   function initialize(uint num) public initializer {
     
     GSNRecipientERC20Fee.initialize("token","tkn");
     _owner = _msgSender();
     count = num;
     _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+            addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+
   }
-  // contract Counter is ERC20, ERC20Detailed, GSNRecipient {
-  // uint private count;
-  // address private _owner;
-    
-  //   constructor (uint num) public ERC20Detailed("SimpleToken", "SIM", 18) {
-  //         _owner = _msgSender();
-  //   count = num;
-  //       _mint(msg.Sender(), 10000 * (10 ** uint256(decimals())));
-  //   }
 
   // accept all requests
   // function acceptRelayedCall(
@@ -54,24 +55,20 @@ contract Counter is Initializable, GSNRecipientERC20Fee ,ERC20,ERC20Detailed {
     return _owner;
   }
 
-  // getter
-  function getCounter() public view returns (uint) {
-    return count;
-  }
+ function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
-  //and it can add to a count
-  function increaseCounter(uint256 amount) public {
-    count = count + amount;
-  }
-
-  //We'll upgrade the contract with this function after deploying it
-  //Function to decrease the counter
-  function decreaseCounter(uint256 amount) public returns (bool) {
-    require(count > amount, "Cannot be lower than 0");
-    count = count - amount;
-    return true;
-  }
-
+    function vote(uint256 _candidateId) public {
+        require(
+            _candidateId <= candidatesCount && _candidateId > 0,
+            "Invalid candidate"
+        );
+        require(!voters[msg.sender], "Vote must not have voted before.");
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
   function setRelayHubAddress() public {
     if(getHubAddr() == address(0)) {
       _upgradeRelayHub(0xD216153c06E857cD7f72665E0aF1d7D82172F494);
@@ -83,21 +80,3 @@ contract Counter is Initializable, GSNRecipientERC20Fee ,ERC20,ERC20Detailed {
   }
 
 }
-// pragma solidity ^0.5.0;
-
-// import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-// //import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
-
-// contract MyNFT is ERC721Full {
-//     // string[] public colors;
-//     // mapping(string => bool) _exist;
-//     constructor() ERC721Full("MyNFT", "MNFT") public {
-//     }
-
-// //     function mint(string memory _color) public {
-// // uint _id=colors.push(_color);
-// // _mint(msg.sender, _id);
-// // _exist[_color]=true;
-// //     }
-
-// }
